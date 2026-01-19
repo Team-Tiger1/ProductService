472f17ddcdb206d259c0cfc46e5d9d27ada9dd6c
Dan Jackson <danieljackson634@gmail.com>
2026-01-19 21:33:37 +0000

    Added description for create product endpoint

--- src/main/java/com/teamtiger/productservice/products/controllers/ProductController.java
+++ src/main/java/com/teamtiger/productservice/products/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.teamtiger.productservice.products.controllers;
 
 import com.teamtiger.productservice.products.models.ProductDTO;
 import com.teamtiger.productservice.products.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ public class ProductController {
     private final ProductService productService;
 
 
+    @Operation(summary = "Allows a Vendor to add a new Product")
     @PostMapping
     public ResponseEntity<?> createProduct(@RequestHeader("Authorization") String authHeader, @RequestBody ProductDTO dto) {
 
