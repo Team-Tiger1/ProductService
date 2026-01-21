b91504a15737afa02d73f5de74b3e388a9c23729
Dan Jackson <danieljackson634@gmail.com>
2026-01-21 16:14:45 +0000

    Added index on vendor_id to increase performance

--- src/main/java/com/teamtiger/productservice/bundles/repositories/BundleRepository.java
+++ src/main/java/com/teamtiger/productservice/bundles/repositories/BundleRepository.java
@@ -3,7 +3,11 @@ package com.teamtiger.productservice.bundles.repositories;
 import com.teamtiger.productservice.bundles.entities.Bundle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BundleRepository extends JpaRepository<Bundle, UUID> {
+
+    List<Bundle> findAllByVendorId(UUID vendorId);
+
 }
