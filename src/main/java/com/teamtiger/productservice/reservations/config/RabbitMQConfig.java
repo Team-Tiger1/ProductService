2a090f0ca51cf918d99e91bd7652b5372fec3b4a
Dan Jackson <danieljackson634@gmail.com>
2026-03-05 18:29:51 +0000

    ID-221 Created Delayed Queue

--- src/main/java/com/teamtiger/productservice/reservations/config/RabbitMQConfig.java
+++ src/main/java/com/teamtiger/productservice/reservations/config/RabbitMQConfig.java
@@ -1,11 +1,14 @@
 package com.teamtiger.productservice.reservations.config;
 
-import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Configuration for publishing reservation related events.
@@ -15,9 +18,33 @@ public class RabbitMQConfig {
 
     public static final String EXCHANGE = "reservation.events";
 
+    public static final String DELAY_EXCHANGE = "reservation.delay";
+    public static final String DELAY_QUEUE = "reservation.delay.queue";
+    public static final String DELAY_ROUTING_KEY = "reservation.delay.key";
+
     @Bean
     public DirectExchange productExchange() {
         return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public CustomExchange delayedExchange() {
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-delayed-type", "direct");
+        return new CustomExchange(DELAY_EXCHANGE, "x-delayed-message", true, false, arguments);
+    }
+
+    @Bean
+    public Queue delayQueue() {
+        return new Queue(DELAY_QUEUE, true);
+    }
+
+    @Bean
+    public Binding delayQueueBinding(Queue delayQueue, CustomExchange delayedExchange) {
+        return BindingBuilder.bind(delayQueue)
+                .to(delayedExchange)
+                .with(DELAY_ROUTING_KEY)
+                .noargs();
     }
 
     //Converts Java objects to JSON
