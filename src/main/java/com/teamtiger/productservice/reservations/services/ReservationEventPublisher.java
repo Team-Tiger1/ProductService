2f637b696df592de4a65e9c9d6a0585c29226d48
Dan Jackson <danieljackson634@gmail.com>
2026-03-05 18:30:20 +0000

    ID-221 Created Publish Method for Delayed Queue

--- src/main/java/com/teamtiger/productservice/reservations/services/ReservationEventPublisher.java
+++ src/main/java/com/teamtiger/productservice/reservations/services/ReservationEventPublisher.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 /**
  * Notifies that a reservation has been collected
  */
@@ -21,6 +23,18 @@ public class ReservationEventPublisher {
             message.getMessageProperties().setContentType("application/json");
             return message;
                 });
+    }
+
+    public void publishNoShowEvent(UUID bundleId, long delay) {
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.DELAY_EXCHANGE,
+                RabbitMQConfig.DELAY_ROUTING_KEY,
+                bundleId,
+                message -> {
+                    message.getMessageProperties().setDelayLong(delay + 100); //+100 ms ensures message is picked up after window has ended
+                    return message;
+                }
+        );
     }
 
 }
