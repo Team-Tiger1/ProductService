44e354e0093b0440765f62238eeeda52d0dda028
Dan Jackson <danieljackson634@gmail.com>
2026-03-10 11:35:17 +0000

    ID-282 Added Stack Trace

--- src/main/java/com/teamtiger/productservice/bundles/controllers/BundleController.java
+++ src/main/java/com/teamtiger/productservice/bundles/controllers/BundleController.java
@@ -173,6 +173,7 @@ public class BundleController {
         }
 
         catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.internalServerError().build();
         }
     }
